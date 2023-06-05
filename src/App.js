88d52fa2f6960c77bbda36074fb081ecd8b2c0d3
phil <philcajurao@gmail.com>
2023-06-05 18:45:42 +0800

    Updated Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BiMenu } from "react-icons/bi";
+import phil from "./images/Phil.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main text-[#111] bg-white caret-transparent">
+      <div className="navbar top-0 w-full border border-black">
+        <div className="fixed flex justify-between w-full">
+          <button className="p-3 font-extrabold">Phil.</button>
+          <div>
+            <BiMenu size={30} className="cursor-pointer sm:hidden p-3" />
+            <ul className="sm:flex hidden ">
+              <li>
+                <button className="p-3 cursor-pointer transition duration-300 hover:bg-[#111] hover:text-[#fefefe]">About me</button>
+              </li>
+              <li>
+                <button className="p-3 cursor-pointer transition duration-300 hover:bg-[#111] hover:text-[#fefefe]">Contacts</button>
+              </li>
+              <li>
+                <button className="p-3 cursor-pointer transition duration-300 hover:bg-[#111] hover:text-[#fefefe]">Projects</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="header flex flex-col items-center h-[90vh] h-[90svh]">
+        <img src={phil} alt="Phil" className="w-96 mt-auto" draggable="false" />
+      </div>
+
+      <div className="content bg-[#111] p-3 bottom-0">
+        <div className="text-[#fefefe] flex flex-col items-center mx-[20vw] ">
+          <div className="flex flex-col items-center mt-2">
+            <h1 className="font-bold">PHIL CAJURAO</h1>
+            <p>&lt;p&gt;Aspiring Developer&lt;/p&gt;</p>
+          </div>
+          <div className="mt-10">
+            <h2 className="text-[#555] text-lg">About Me</h2>
+            <div className="flex flex-col space-y-5 mt-2 text-justify">
+              <p className="">
+                Hi! My name's <b>Phil</b>, currently a college student aspiring
+                to be a great developer. I've met programming in an interesting
+                and unusual way that brought me wanting to know it more.
+              </p>
+              <p className="">
+                Along with my school projects, I did some freelancing alone or
+                together with my friends to earn more exciting experience. This
+                faced me with common problems and bugs that helped me learn a
+                lot.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
