2b29e059eee0b043f22b5aa104ef08b5109bf60c
phil <philcajurao@gmail.com>
2023-06-06 01:30:13 +0800

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       onContextMenu={handleContextMenu}
       onDoubleClick={handleDoubleClick}
     >
-      <div className="navbar top-0 w-full">
+      <div className="navbar top-0 w-full z-20">
         <div className="fixed flex px-7 justify-between w-full">
           <button className="p-3 font-extrabold">Phil.</button>
           <div className="flex">
@@ -48,7 +48,7 @@ function App() {
       <div className="fixed inset-0 z-[-1]">
         <img src={bg} alt="Background" className="w-full h-full object-cover" />
       </div>
-      <div className="header flex justify-center h-[90vh] h-[90svh] relative z-10">
+      <div className="header flex justify-center sm:h-[87vh] sm:h-[87svh] h-[90vh] h-[90svh] ">
         <img
           src={phil}
           alt="Phil"
@@ -60,26 +60,30 @@ function App() {
       <div className="content bg-[#111] p-3 bottom-0 z-20">
         <div className="text-[#fefefe] flex flex-col items-center mx-[20vw]">
           <div className="flex flex-col items-center mt-2">
-            <h1 className="font-bold">PHIL CAJURAO</h1>
-            <p>&lt;p&gt;Aspiring Developer&lt;/p&gt;</p>
+            <h1 className="font-bold sm:text-5xl text-2xl">PHIL CAJURAO</h1>
+            <p className="sm:text-xl text-base sm:mt-2">&lt;p&gt;Aspiring Developer&lt;/p&gt;</p>
           </div>
           <div className="mt-10">
             <h2 className="text-[#555] text-lg">About Me</h2>
             <div className="flex flex-col space-y-5 mt-2 text-justify">
               <p className="">
-                Hi! My name's <b>Phil</b>, currently a college student aspiring
+                Hi! My name's <b>Phil</b>, currently a graduating college student aspiring
                 to be a great developer. I've met programming in an interesting
                 and unusual way that brought me wanting to know it more.
               </p>
               <p className="">
                 Along with my school projects, I did some freelancing alone or
                 together with my friends to earn more exciting experience. This
-                faced me with common problems and bugs that helped me learn a
+                faced me with common problems and bugs that helped me learned a
                 lot.
               </p>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="footer bg-[#222] text-[#fefefe] flex justify-center p-3">
+        <p>© Phil Cajurao 2023</p>
       </div>
     </div>
   );
