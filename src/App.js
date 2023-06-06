8525d4b475802de055f07dca62b8bede52e3ec21
phil <philcajurao@gmail.com>
2023-06-06 11:25:26 +0800

    fixinf background animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BiMenu } from "react-icons/bi";
 import phil from "./images/Phil.png";
 import bg from "./images/Background.png";
+import bg1 from "./images/1.png";
+import bg2 from "./images/2.png";
 import NavBar from "./components/NavBar";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
@@ -22,8 +24,11 @@ function App() {
     >
       <NavBar />
       <div className="fixed inset-0 z-[-1]">
-        <img src={bg} alt="Background" className="w-full h-full object-cover" />
+        {/* <img src={bg} alt="Background" className="w-full h-full object-cover" /> */}
+        <img src={bg1} alt="Background" className="w-full h-full object-cover absolute left-0" />
+        <img src={bg2} alt="Background" className="w-full h-full object-cover absolute right-0" />
       </div>
+      {/* motion-safe:animate-translateRight */}
       <div className="header flex justify-center sm:h-[87vh] sm:h-[87svh] h-[90vh] h-[90svh] ">
         <img
           src={phil}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards",
+        translateRight: "translateRight 2s ease-in forwards",
+        translateLeft: "translateLeft 2s ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        },
+        translateRight: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" }
+        },
+        translateLeft: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" }
+        }
+      }
+    },
   },
+  variants: {
+    animation: ["motion-safe"]
+},
   plugins: [],
 }
 
