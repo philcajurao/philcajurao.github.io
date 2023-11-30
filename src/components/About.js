c24547ae02752e76e284929c4753f91e907a6c5b
phil <philcajurao@gmail.com>
2023-12-01 00:37:24 +0800

    to be fix

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,14 @@ function About() {
       <h2 className="text-[#fefefe] text-lg text-center">About Me</h2>
       <div className="flex flex-col space-y-5 mt-2 text-justify text-[#777]">
         <p className="">
-          Hi! My name's <b>Phil</b>, currently a graduating college student
-          based in the Philippines. I've met programming in an
+          Hi! My name's <b>Phil</b>, based in the Philippines and currently working remotely as a 
+          Web Developer outside my country. I've met programming in an
           interesting and unusual way that brought me wanting to know it more.
         </p>
         <p className="">
-          Along with my school projects, I did some freelancing alone or
-          together with my friends to earn more exciting experience. This faced
-          me with common problems and bugs that helped me learned a lot.
+        I specialize in front-end and back-end development, and I’m proficient in various programming
+         languages and frameworks. I’m always eager to learn more and stay updated with the latest 
+         industry trends and advancements.
         </p>
       </div>
       <div className="w-full flex justify-center mt-5">
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,14 @@
 import React from "react";
-import ursaconline from "../images/URSACOnline.png";
 import porpiks1word from "../images/4pics1word.png";
 import churchsite from "../images/ChurchWebsite.png";
-import cinemaseats from "../images/CinemaSeats.png";
-import dictionary from "../images/Dictionary.png";
-import greenwichclone from "../images/GreenwichClone.png";
 import employeedatabase from "../images/EmployeeDatabase.png";
-import gurjarmaps from "../images/GurjarMaps.png";
 import reactLogo from '../icons/react.png'
 import htmlLogo from '../icons/html.png'
 import cssLogo from '../icons/css.png'
 import jsLogo from '../icons/js.png'
-import tailwindLogo from '../icons/tailwind.png'
 import muiLogo from '../icons/mui.png'
 import javaLogo from '../icons/java.png'
-import phpLogo from '../icons/php.png'
-import materializeLogo from '../icons/materialize.png'
 import mysqlLogo from '../icons/mysql.png'
-import apiLogo from '../icons/api.png'
 
 function Project() {
   return (
@@ -27,107 +18,8 @@ function Project() {
 
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
 
-      <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
-        {/* Card Container */}
-        <div className="">
-          <img
-            src={greenwichclone}
-            alt="Greenwich Clone"
-          />
-        </div>
-        {/* Card Content */}
-        <div className="text-[#fefefe] flex flex-col p-5 bg-[#222] h-[30rem]">
-          <div className="my-2">
-            Greenwich POS Clone
-          </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
-              A mimicked Point of Sale System specifically designed for Greenwich, featuring both admin and cashier accounts, based on a cloned Greenwich UI.
-          </div>
-          <div className="my-4">
-            Tech Stack
-            <ul className='flex flex-wrap mt-1'>
-                <li><img src={htmlLogo} alt='HTML Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={cssLogo} alt='CSS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={jsLogo} alt='JS Logo' className='m-1 w-5 h-5' /></li>
-                <li className='flex items-center'><img src={materializeLogo} alt='Materialize Logo' className='m-1 w-5 h-auto' /></li>
-                <li><img src={phpLogo} alt='PHP Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={mysqlLogo} alt='MySQL Logo' className='m-1 w-8 h-5' /></li>
-            </ul>
-          </div>
-          <div className="mt-10 mb-10 flex flex-col sm:flex-row space-y-5 sm:space-y-0 justify-evenly sm:mt-auto">
-            <a href="https://github.com/philcajurao/greenwich-pos-clone.git" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Code</button></a>
-            {/* <a href="#"><button className="emailBtn w-full">Demo</button></a> */}
-          </div>
-        </div>
-      </div>
 
-      <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
-        {/* Card Container */}
-        <div className="">
-          <img
-            src={cinemaseats}
-            alt="Cinema Seats"
-          />
-        </div>
-        {/* Card Content */}
-        <div className="text-[#fefefe] flex flex-col p-5 bg-[#222] h-[30rem]">
-          <div className="my-2">
-            Cinema Seats
-          </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
-            This website serves as a cinema reservation system, enabling users to choose a movie and purchase tickets.
-          </div>
-          <div className="my-4">
-            Tech Stack
-            <ul className='flex flex-wrap mt-1'>
-                <li><img src={htmlLogo} alt='HTML Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={cssLogo} alt='CSS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={jsLogo} alt='JS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={phpLogo} alt='PHP Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={mysqlLogo} alt='MySQL Logo' className='m-1 w-8 h-5' /></li>
-            </ul>
-          </div>
-          <div className="mt-10 mb-10 flex flex-col sm:flex-row space-y-5 sm:space-y-0 justify-evenly sm:mt-auto">
-            <a href="https://github.com/philcajurao/cinema-seats.git" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Code</button></a>
-            {/* <a href="#"><button className="emailBtn w-full">Demo</button></a> */}
-          </div>
-        </div>
-      </div>
-
-      <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
-        {/* Card Container */}
-        <div className="">
-          <img
-            src={dictionary}
-            alt="Dictionary"
-          />
-        </div>
-        {/* Card Content */}
-        <div className="text-[#fefefe] flex flex-col p-5 bg-[#222] h-[30rem]">
-          <div className="my-2">
-            Dictionary
-          </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
-            Website that is using Free Dictionary API I got from&nbsp;
-            <a href="https://publicapis.dev" target='_blank' rel="noopener noreferrer" className="cursor-pointer underline">https://publicapis.dev</a> 
-            &nbsp;that makes the dictionary functionable providing informations and even phonetics.
-          </div>
-          <div className="my-4">
-            Tech Stack
-            <ul className='flex flex-wrap mt-1'>
-                <li><img src={htmlLogo} alt='HTML Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={cssLogo} alt='CSS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={jsLogo} alt='JS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={apiLogo} alt='API Logo' className='m-1 w-5 h-5' /></li>
-            </ul>
-          </div>
-          <div className="mt-10 mb-10 flex flex-col sm:flex-row space-y-5 sm:space-y-0 justify-evenly sm:mt-auto">
-            <a href="https://github.com/philcajurao/dictionary.git" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Code</button></a>
-            <a href="https://tourmaline-gumption-e52c40.netlify.app/" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Demo</button></a>
-          </div>
-        </div>
-      </div>
-
+      {/* PORPIKS WAN WORD */}
       <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
         {/* Card Container */}
         <div className="">
@@ -141,7 +33,7 @@ function Project() {
           <div className="my-2">
             4pics1word
           </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
+          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
             An exciting 4 Picture 1 Word game that puts players into guessing of a word by a 4 picture given the set of letters they have and congratulate them in the end.
           </div>
           <div className="my-4">
@@ -159,6 +51,8 @@ function Project() {
         </div>
       </div>
 
+
+      {/* CHURCH SITE */}
       <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
         {/* Card Container */}
         <div className="">
@@ -172,7 +66,7 @@ function Project() {
           <div className="my-2">
             Church Landing Page
           </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
+          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
             Landing website for a modern church featuring their gatherings, ministries and other informations.
           </div>
           <div className="my-4">
@@ -192,76 +86,8 @@ function Project() {
         </div>
       </div>
 
-      <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
-        {/* Card Container */}
-        <div className="">
-          <img
-            src={gurjarmaps}
-            alt="Gurjar Maps"
-          />
-        </div>
-        {/* Card Content */}
-        <div className="text-[#fefefe] flex flex-col p-5 bg-[#222] h-[30rem]">
-          <div className="my-2">
-            Gurjar Maps
-          </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
-            A system where you can find gurjar people and their basic info around the world built using React-Leaflet, OpenStreetMap and Maptiler for the map's layer.
-          </div>
-          <div className="my-4">
-            Tech Stack
-            <ul className='flex flex-wrap mt-1'>
-                <li><img src={htmlLogo} alt='HTML Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={cssLogo} alt='CSS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={jsLogo} alt='JS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={reactLogo} alt='React Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={tailwindLogo} alt='Tailwind Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={apiLogo} alt='API Logo' className='m-1 w-5 h-5' /></li>
-            </ul>
-          </div>
-          <div className="mt-10 mb-10 flex flex-col sm:flex-row space-y-5 sm:space-y-0 justify-evenly sm:mt-auto">
-            <a href="https://github.com/RinaCLDS/Big-project.git" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Code</button></a>
-            <a href="https://rainbow-kataifi-337b7f.netlify.app" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Demo</button></a>
-          </div>
-        </div>
-      </div>
 
-      <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
-        {/* Card Container */}
-        <div className="">
-          <img
-            src={ursaconline}
-            alt="URSAC Online Logo"
-          />
-        </div>
-        {/* Card Content */}
-        <div className="text-[#fefefe] flex flex-col p-5 bg-[#222] h-[30rem]">
-          <div className="my-2">
-            URSAC Online
-          </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
-            A Learning Management System for University of Rizal System Antipolo Campus, capable of creating and joining course subjects created by users and post announcements or activities with an option of having pdf file
-            on attachment. Users can edit their details such us profile photo, password, and basic info. Registration of users can only be done by the website's administration page.
-          </div>
-          <div className="my-4">
-            Tech Stack
-            <ul className='flex flex-wrap mt-1'>
-                <li><img src={htmlLogo} alt='HTML Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={cssLogo} alt='CSS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={jsLogo} alt='JS Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={reactLogo} alt='React Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={muiLogo} alt='MUI Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={phpLogo} alt='PHP Logo' className='m-1 w-5 h-5' /></li>
-                <li><img src={mysqlLogo} alt='Java Logo' className='m-1 w-8 h-5' /></li>
-            </ul>
-          </div>
-          <div className="mt-10 mb-10 flex flex-col sm:flex-row space-y-5 sm:space-y-0 justify-evenly sm:mt-auto">
-            <a href="#" className="sm:w-auto w-full"><button className="emailBtn w-full">Code</button></a>
-            <a href="https://www.ursaconline.com" target='_blank' rel="noopener noreferrer" className="sm:w-auto w-full"><button className="emailBtn w-full">Demo</button></a>
-          </div>
-        </div>
-      </div>
-
+      {/* EMPLOYEE DATABASE */}
       <div className="text-[#111] w-full max-w-[400px] mx-auto my-5">
         {/* Card Container */}
         <div className="">
@@ -276,7 +102,7 @@ function Project() {
           <div className="my-2">
             Employee Database
           </div>
-          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
+          <div className="text-sm text-[#777] my-2 h-24 overflow-y-auto scrollbar-thin scrollbar-thumb-[#1a1a1a] scrollbar-track-[rgba(255,255,255,0.1)]">
            A basic management system for employees where you can manage their data by editing, deleting, and adding new employee. You can also view all the employees by the table 
            and can navigate with next and previous buttons. Built using Netbeans and Java Swing.
            </div>
